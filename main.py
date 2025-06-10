09c38a99c28ed0c0c164437adede9274b49b1bbc
Александр Лисак <lisak@MacBook-Air-Aleksandr.local>
2025-06-10 19:33:56 +0300

    Первый коммит

---
+++ main.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+import asyncio
+
+API_TOKEN = '8183661988:AAHucFqlurcSvaLbkD8ZyI0-JAYSsF6xU_U'
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command(commands=["start"]))
+async def start_handler(message: types.Message):
+    await message.answer("Привет, братец! Я Санин первый бот 🤖, это чисто тестик")
+
+@dp.message()
+async def echo_handler(message: types.Message):
+    await message.answer(f"Ты сказал: {message.text}")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
